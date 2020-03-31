bcda38e1e25d8004cf3b132eaf5457f28cf48954
Luther Jensen <luther.jensen98@gmail.com>
2020-03-31 13:31:23 -0500

    Did some work on the layout and styling for the overall site. Added in some javascript and the jquery UI for the create recipe page.

---
+++ PalmTreeRecipe/PalmTreeRecipe/wwwroot/js/CreateRecipe.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+
+    $("#ingredientDialog").hide();
+
+    //add the modal for adding an ingredient to a recipe
+    $("#btnAddIngredient").click(function () {
+        $("#ingredientDialog").dialog({
+            buttons: [
+                {
+                    text: "Add",
+                    click: function () {
+                        $(this).dialog("close");
+                    }
+                },
+                {
+                    text: "Cancel",
+                    click: function () {
+                        $(this).dialog("close");
+                    }
+                }
+            ],
+            modal: true
+        });
+    });
+
+    //if they click to add a step we want to show the add step modal
+    $("#btnAddStep").click(function () {
+
+    });
+});
